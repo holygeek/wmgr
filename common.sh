14923e1e248906a8e9de1526c62fbec3a3734b13
nazri <ayiehere@gmail.com>
2012-10-02 11:40:34 +0800

    add common.sh

---
+++ common.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+debug() {
+  test -z "$debug" && return
+  field="$1";
+  shift
+  printf "%10s DEBUG: %12s: %s\n" "$me" "$field" "$*"
+}
+
+bail() {
+  echo $*
+  if [ -n "$zenity" ]; then
+    zenity --error --text "$*"
+  fi
+  exit 1
+}
+
+bail_if_empty() {
+  test -z "$1" && bail $2
+}
+
+exe() {
+  if [ -n "$dry_run" ]; then
+    echo "$@"
+  else
+    if [ -n "$debug" ]; then
+      echo " $me EXE: $@" 1>&2
+    fi
+    sh -c "$*"
+  fi
+}
