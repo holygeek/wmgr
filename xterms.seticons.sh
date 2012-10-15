03f969ec75ac3eea60d1ec904980311fe610d7e4
nazri <ayiehere@gmail.com>
2012-10-15 16:31:51 +0800

    variable-ize tmp dir location

--- xterms.seticons.sh
+++ xterms.seticons.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # xterms.seticons.sh - Set icons for xterms
 # Created: Wed Feb 15 11:23:53 MYT 2012
+tmp_dir=/tmp
 cwd=${cwd:-$HOME/wmgr}
-for i in /tmp/term.*.winid; do
+for i in $tmp_dir/term.*.winid; do
     window_id=`cat $i`
-    xterm_short_id=`echo $i|sed -e 's,/tmp/term.\(.\).*,\1,'`
+    xterm_short_id=`echo $i|sed -e 's,.*/term.\(.\).*,\1,'`
     icon_png=$cwd/xterms.d/${xterm_short_id}.png
     $cwd/xseticon -id $window_id  $icon_png
 done
