61b0362d19ebd9ccebf3214576c8539cf1cead62
nazri <ayiehere@gmail.com>
2012-11-12 12:03:53 +0800

    xterms.seticons.sh - announce if xseticon errored out

--- xterms.seticons.sh
+++ xterms.seticons.sh
@@ -7,5 +7,5 @@ for i in $tmp_dir/term.*.winid; do
     window_id=`cat $i`
     xterm_short_id=`echo $i|sed -e 's,.*/term.\(.\).*,\1,'`
     icon_png=$cwd/xterms.d/${xterm_short_id}.png
-    $cwd/xseticon -id $window_id  $icon_png
+    $cwd/xseticon -id $window_id  $icon_png || echo "(xseticon error)"
 done
