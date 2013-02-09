3ea1ee868dd4c50b67d537e7a466e9dc6d78e490
Nazri Ramliy <ayiehere@gmail.com>
2013-02-09 15:20:51 +0800

    xterms.seticons.sh - remove hardcoded wmgr path

--- xterms.seticons.sh
+++ xterms.seticons.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 # xterms.seticons.sh - Set icons for xterms
 # Created: Wed Feb 15 11:23:53 MYT 2012
+cwd=`dirname $0`;
 tmp_dir=/dev/shm
-cwd=${cwd:-$HOME/wmgr}
 for i in $tmp_dir/term.*.winid; do
     window_id=`cat $i`
     xterm_short_id=`echo $i|sed -e 's,.*/term.\(.\).*,\1,'`
     icon_png=$cwd/xterms.d/${xterm_short_id}.png
     if [ -f $icon_png ]; then
-	$cwd/xseticon -id $window_id $icon_png || echo "(xseticon error)"
+	xseticon -id $window_id $icon_png || echo "(xseticon error)"
     else
 	echo "$icon_png DNE" >&2
     fi
