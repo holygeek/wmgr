c80080dba9e6115c31512e39707aa6a45d38aea2
Nazri Ramliy <ayiehere@gmail.com>
2013-02-09 12:36:40 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+scripts="$(find . -maxdepth 1 -type f -perm /100|sed -e 's,\./,,') common.sh"
+rm -f installed.txt
+log_installed() {
+  echo $1 >> installed.txt
+}
+
+make_symlink() {
+  symlink=$1
+  target=$PWD/$2
+
+  if [ -e $symlink -a ! -L $symlink ]; then
+    echo " x  not a symlink: $symlink";
+    continue;
+  fi;
+  if [ -L $symlink ]; then
+    if [ "`readlink -e $symlink`" = "$target" ]; then
+      log_installed $symlink
+      echo "✓   $symlink";
+    else
+      echo " x  `file $symlink`";
+    fi;
+  else
+    log_installed $symlink
+    echo "✓   ln -s $target $symlink";
+    ln -s $target $symlink;
+  fi;
+}
+
+# Symlink scripts to ~/bin
+for s in $scripts; do
+  make_symlink ~/bin/$s $s
+done
+make_symlink ~/.xinbkeysrc xbindkeysrc
+
+# Symlink openbox config files
+openbox_dir=~/.config/openbox
+mkdir -p $openbox_dir
+make_symlink $openbox_dir/menu.xml openbox-menu.xml
+make_symlink $openbox_dir/rc.xml openbox-rc.xml
+
+# Install xterms.conf.pl
+if [ ! -f xterms.conf.pl ]; then
+  cp -v xterms.conf.pl.sample xterms.conf.pl
+fi
+
+# Check for dependencies
+for i in wmctrl xbindkeys dmenu xftmetric; do
+  if ! which $i > /dev/null; then
+    echo " x  not installed: $i"
+  fi
+done
+
+# Check xbindkeys key bindings
+keys=`cat xterms.conf.pl|sed -n -e '/=> join/s/^ *\([a-z]\).*/\1/p'`
+for c in $keys; do
+  if ! grep -q "Alt + $c" xbindkeysrc; then
+    echo " x  missing xbindkeys entry for 'Alt + $c'"
+  fi
+done
--- makefile
+++ makefile
@@ -1,35 +1,10 @@
-scripts=$(shell find . -maxdepth 1 -type f -perm /100|sed -e 's,\./,,')
-
 install:
-	@for s in $(scripts); do \
-	  symlink=~/bin/$$s; \
-	  target=$(PWD)/$$s; \
-	  if [ -e $$symlink -a ! -L $$symlink ]; then \
-	    echo "x   not a symlink: $$symlink"; \
-	    continue; \
-	  fi; \
-	  if [ -L $$symlink ]; then \
-	    if [ "`readlink -e $$symlink`" = "$$target" ]; then \
-	      echo "✓   $$symlink"; \
-	    else \
-	      echo "x   `file $$symlink`"; \
-	    fi; \
-	  else \
-	    echo "✓   ln -s $$target $$symlink"; \
-	    ln -s $$target $$symlink; \
-	  fi; \
-	done
+	sh install.sh
 
 uninstall:
-	@for s in $(scripts); do \
-	  symlink=~/bin/$$s; \
-	  target=$(PWD)/$$s; \
-	  if [ -L $$symlink ]; then \
-	    if [ "`readlink -e $$symlink`" = "$$target" ]; then \
-	      echo "✓   removing: $$symlink"; \
-	      rm -f $$symlink; \
-	    else \
-	      echo "x   left alone: $$symlink"; \
-	    fi; \
-	  fi; \
-	done
+	@test -f installed.txt || { echo "No installed.txt"; exit 1; }
+	@cat installed.txt|while read symlink; do \
+	  rm -f $$symlink && \
+	  echo "✓   removing: $$symlink" || echo " x  removing: $$symlink"; \
+	done && \
+	rm -f installed.txt
